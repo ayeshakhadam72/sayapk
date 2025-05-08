977b1c7bdd39bb00b3d467c4b0dbcbb2e1eb509b
Ayesha <ayeshakhadam2@gmail.com>
2025-05-08 02:27:58 -0700

    ayesha

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,7 +11,7 @@ import Logo from '../../public/logosaya.png';
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 left-0 w-full z-50 flex justify-between px-6 py-4  bg-opacity-50   items-end">
+    <div className="fixed top-0 left-0 w-full z-50 flex gap-10 justify-between pl-6 pr-2 py-4  bg-opacity-50   items-end">
       {/* Left: Logo */}
       <div className="flex items-end space-x-2">
         <Image src={Icon} alt="Icon" width={20} height={20} className="pb-1" />
@@ -20,22 +20,22 @@ const Navbar = () => {
 
       {/* Right: Navigation */}
       <nav>
-        <ul className="flex space-x-6 text-black uppercase text-[11px] items-center">
+        <ul className="flex space-x-4 text-black uppercase text-[11px] items-center">
           <li>
             <Link href="/" className="flex items-center space-x-1">
-              <CiSearch className="text-xl md:hidden mb-[-2px] "  />
+              <CiSearch className="text-xl md:hidden mt-[-10px] "  />
               <span className="hidden md:inline">search</span>
             </Link>
           </li>
           <li>
             <Link href="/" className="flex items-center space-x-1">
-              <VscAccount className="text-xl md:hidden" />
+              <VscAccount className="text-xl md:hidden mt-[-10px]" />
               <span className="hidden md:inline">account</span>
             </Link>
           </li>
           <li>
             <Link href="/" className="flex items-center space-x-1">
-              <CiShoppingCart  className="text-2xl md:hidden mb-[-3px]" />
+              <CiShoppingCart  className="text-2xl md:hidden mt-[-10px]" />
               <span className="hidden md:inline">wishlist</span>
             </Link>
           </li>
--- src/components/whatsppicon.js
+++ src/components/whatsppicon.js
@@ -6,7 +6,8 @@ const WhatsApp = () => {
   return (
     <div className="fixed bottom-6 right-6 z-50">
       <a 
-        // href="https://wa.me/yournumber" 
+        // href="https://wa.me/Chat on WhatsApp with +92 300 7887292" 
+       
         target="_blank" 
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 z-50 bg-green-500 hover:bg-green-600 p-3 rounded-full shadow-lg transition"
